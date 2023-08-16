b10e9c100d605c80776d638ace32ac4443bbd3f8
Don Boyd <donboyd5@gmail.com>
2023-08-16 09:36:18 -0400

    WIP

---
+++ r/libraries.R
@@ -0,0 +1,38 @@
+
+library(tidyverse)
+options(tibble.print_max = 60, tibble.print_min = 60) # if more than 60 rows, print 60 - enough for states
+# ggplot2 tibble tidyr readr purrr dplyr stringr forcats
+library(readr)
+library(vroom)
+library(readxl)
+library(arrow)
+library(fs)
+
+library(scales)
+library(hms) # hms, for times
+library(lubridate) # lubridate, for date/times
+library(vctrs)
+library(skimr)
+
+library(grDevices)
+library(knitr)
+library(kableExtra)
+library(ggrepel)
+
+# remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch")
+library(tabulizer)
+
+# my packages
+# renv::install(here::here("renv/cellar/btools_0.9.5.tar.gz"))
+library(btools)
+# renv::install(here::here("renv/cellar/bggtools_0.0.9200.tar.gz"))
+library(bggtools)
+
+library(DT) # for datatable
+
+library(zoo) # for rollapply
+
+library(bea.R)
+library(fredr)
+library(httr)
+library(jsonlite)
